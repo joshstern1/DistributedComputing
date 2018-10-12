ed1f88382cc5dacabfafa833f30ab6029a3562f3
vaibhavb1996 <vbansal@bu.edu>
2018-10-12 15:23:07 -0400

    made simple hello world server

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
 # This is the basic hello world server
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST', 'HEAD', 'OPTIONS', 'PUT', 'DELETE'])
+def F():
+	if request.data:
+		name = request.get_json()["name"]
+	else:
+		name = "World"
+	return 'Hello '+name+' !'
+
+if __name__=='__main__':
+	app.run(host='0.0.0.0', port=5000)
