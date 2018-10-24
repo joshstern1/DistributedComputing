1bda901f89d67aba7e514f0590e22f900a3cc34c
joshstern1 <jstern19@bu.edu>
2018-10-24 13:51:36 -0400

    updated client.py

--- docker/client.py
+++ docker/client.py
@@ -1,5 +1,8 @@
 import requests
 
+#used to simulate the master server, which acts as a client when communicating with docker containers on worker nodes
+#sends a post request to a docker container containing a function to be run, then receive and print the function output
+ 
 url = "http://localhost:8000/hello"
 fin = open('testing.py', 'r')
 files = {'file': fin}
