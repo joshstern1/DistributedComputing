3134d5364de95d2e4d640c4ec25dc8de25bfb6e8
Joshua Stern <jstern19@bu.edu>
2018-11-11 21:23:31 -0500

    Update client.py

--- docker/client.py
+++ docker/client.py
@@ -4,7 +4,7 @@ import deploy_app
 #used to simulate the master server, which acts as a client when communicating with docker containers on worker nodes
 #sends a post request to a docker container containing a function to be run, then receive and print the function output
 
-sudoPassword = 'BakeJeep10'
+sudoPassword = ''
 
 def startup():
     cmd = "python deploy_app.py"
