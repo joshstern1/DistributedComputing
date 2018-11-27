93f04da400690b157d8afd22e933904b75b0ed32
Joshua Stern <jstern19@bu.edu>
2018-11-27 15:24:37 -0500

    Update deploy_app.py

--- docker/deploy_app.py
+++ docker/deploy_app.py
@@ -4,12 +4,11 @@ try:
 
     os.system("kubectl run hello-world --image=gcr.io/api-project-vision/hello-world --port=8080")
     time.sleep(5)
-    os.system('kubectl expose deployment/hello-world --type="NodePort" --port 8080')
+    os.system('kubectl expose deployment/hello-world --type="ClusterIP" --port 8080')
     time.sleep(5)
 
+    os.system("sudo kubectl get services/hello-world -o go-template='{{(index .spec.clusterIP)}}' > out1.txt")
 
-    os.system("sudo kubectl get services/hello-world -o go-template='{{(index .spec.ports 0).nodePort}}' > out1.txt")
-    os.system("sudo minikube ip > out2.txt")
 
 except:
     print("Error starting service")
