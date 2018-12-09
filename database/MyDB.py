a1f1bfecb8e61749e05689844abd4d6107c737a4
SeanNonnemacher <seann@bu.edu>
2018-12-09 15:02:58 -0500

    Updated IDs and changed binary export of files

--- database/MyDB.py
+++ database/MyDB.py
@@ -1,41 +1,90 @@
 import mysql.connector
 import re
+import random
+
+
+#TODO
+#Add thread pool to handle multiple connections to the server
+
 
 class MyDB():
     
-    
     def __init__(self):
-        self.my_db = mysql.connector.connect(
-                host = "IP",
-                port = "PORT",
-                user = "root",
-                password = "PASSWORD",
-                database = "distcompschema"
-        )    
-        self.my_cursor = self.my_db.cursor()
-        
-        
+        try:
+            self.my_db = mysql.connector.connect(
+                    host = "IP",
+                    port = "PORT",
+                    user = "root",
+                    password = "PASSWORD",
+                    database = "distcompschema"
+            )
+            self.my_cursor = self.my_db.cursor()
+        except Exception as e:
+            print(e)
+
+
     #Diconnects from database
     def close_connection(self):
-        self.my_db.disconnect()
-        
-    
+        try:
+            self.my_db.disconnect()
+        except:
+            print("disconnection error")
+
+
     #Attempts to add new user
     #Returns userID if successful and False if username already used
     def add_new_user(self, username, password):
         if (not self.check_if_username_present(username)) \
                     and MyDB.check_email_format(username):
+            userID = self.get_new_userID()
             self.my_cursor.execute("""
-                INSERT INTO user (username, password)
-                VALUES (%s, %s)
-            """, (username, password))
-            self.my_cursor.execute("""
-                SELECT LAST_INSERT_ID()
-                """)
-            result = self.my_cursor.fetchone()[0]
+                INSERT INTO user (username, password, userID)
+                VALUES (%s, %s, %s)
+            """, (username, password, userID))
             self.my_db.commit()
+            result = userID
         else:
             result = False
+        return result
+    
+    
+    def get_new_userID(self):
+        userID = random.randint(0, 100000000)
+        while(self.check_userID_present(userID)):
+            userID = random.randint(0, 100000000)
+        return userID
+    
+    
+    def check_userID_present(self, userID):
+        self.my_cursor.execute("""
+            SELECT 1
+            FROM user
+            WHERE userID = %d
+        """ %(userID))
+        if self.my_cursor.fetchone() == None:
+            result = False
+        else:
+            result = True
+        return result
+    
+    
+    def get_new_executableID(self):
+        executableID = random.randint(0, 100000000)
+        while(self.check_executableID_present(executableID)):
+            executableID = random.randint(0, 100000000)
+        return executableID
+    
+    
+    def check_executableID_present(self, executableID):
+        self.my_cursor.execute("""
+            SELECT 1
+            FROM executables
+            WHERE executableID = %d
+        """ %(executableID))
+        if self.my_cursor.fetchone() == None:
+            result = False
+        else:
+            result = True
         return result
     
     
@@ -140,14 +189,30 @@ class MyDB():
         return result
     
     
+    #TODO deleta all attached file and results if not already being done
     #Deletes given user
     def delete_user(self, userID):
         self.my_cursor.execute("""
             DELETE FROM user
             WHERE userID = %d
         """ %(userID))
+        
+        #self.my_cursor.execute("""
+        #    DELETE FROM executables
+        #    WHERE userID = %d
+        #""" %(userID))
         self.my_db.commit()
         
+        
+    #TODO test to ensure it works
+    #Deletes given executableID row from the table
+    def delete_executable(self, executableID):
+        self.my_cursor.execute("""
+            DELETE FROM executables
+            WHERE executableID = %d
+        """ %(executableID))
+        self.my_db.commit()
+
     
     #Returns data in provided file
     @staticmethod
@@ -165,7 +230,7 @@ class MyDB():
     def write_file(data, filepath):
         try:
             with open(filepath, 'wb') as f:
-                f.write(data.encode())
+                f.write(data)
         except Exception as e:
             print("write_file error")
     
@@ -173,16 +238,13 @@ class MyDB():
     #Adds new executable to database returns executable ID
     def add_executable(self, userID, executable_name, executable):
         try:
+            executableID = self.get_new_executableID()
             self.my_cursor.execute("""
-                INSERT INTO executables (userID, executableName, executable)
-                VALUES (%s, %s, %s)
-            """, (userID, executable_name, executable))
-            self.my_cursor.execute("""
-                SELECT LAST_INSERT_ID()
-            """)
-            result = self.my_cursor.fetchone()[0]
+                INSERT INTO executables (userID, executableName, executable, executableID)
+                VALUES (%s, %s, %s, %s)
+            """, (userID, executable_name, executable, executableID))
             self.my_db.commit()
-            return result
+            return executableID
         except:
             print("add_executable error")
     
@@ -190,16 +252,14 @@ class MyDB():
     #Adds new executable from file path to database returns executable ID
     def add_executable_from_file(self, userID, executable_name, filepath):
         try:
+            executableID = self.get_new_executableID()
             data = MyDB.read_file(filepath)
             self.my_cursor.execute("""
-                INSERT INTO executables (userID, executableName, executable)
-                VALUES (%s, %s, %s)
-            """, (userID, executable_name, data))
-            self.my_cursor.execute("""
-                SELECT LAST_INSERT_ID()
-            """)
-            result = self.my_cursor.fetchone()[0]
+                INSERT INTO executables (userID, executableName, executable, executableID)
+                VALUES (%s, %s, %s, %s)
+            """, (userID, executable_name, data, executableID))
             self.my_db.commit()
+            result = executableID
             return result    
         except:
             print("add_executable_from_file error")
@@ -226,7 +286,7 @@ class MyDB():
                 FROM executables
                 WHERE executableID = %d
             """ %(executableID))
-            data = self.my_cursor.fetchone()[0]
+            data = self.my_cursor.fetchone()[0].encode()
             MyDB.write_file(data, filepath)             
         except:
             print("save_executable_to_path error")
@@ -276,7 +336,7 @@ class MyDB():
     
     
     #Returns a list of executables for given user
-    #Returns in form (executable_name, executableID)
+    #Returns if form (executable_name, executableID)
     def get_users_executables(self, userID):
         self.my_cursor.execute("""
             SELECT executableName, executableID
@@ -296,7 +356,7 @@ class MyDB():
         return self.my_cursor.fetchone()[0]
     
     
-    #Update result File from file
+    #Update result File
     def set_result_from_file(self, executableID, filepath):
         try:
             data = MyDB.read_file(filepath)
@@ -335,12 +395,18 @@ class MyDB():
                 FROM executables
                 WHERE executableID = %d
             """ %(executableID))
-            return self.my_cursor.fetchone()[0].encode()
+            result = self.my_cursor.fetchone()[0]
+            if result == None:
+                result = False
+            else:
+                result = result.encode()
+            return result 
         except:
             print("get_result error")
     
     
-    #Returns result File or False if one does not exist
+    #Saves result to given filepath
+    #Returns whether or not result file was present
     def save_result_to_file(self, executableID, filepath):
         try:
             self.my_cursor.execute("""
@@ -348,8 +414,13 @@ class MyDB():
                 FROM executables
                 WHERE executableID = %d
             """ %(executableID))
-            data = self.my_cursor.fetchone()[0]
-            MyDB.write_file(data, filepath)             
+            data = self.my_cursor.fetchone()[0].encode()
+            if data == None:
+                result = False
+            else:
+                result = True
+                MyDB.write_file(data, filepath)
+            return result
         except:
             print("save_result_to_file error")
     
