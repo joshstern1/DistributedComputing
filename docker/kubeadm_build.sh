06a27698308c7785ab554d7f89f0bcb216e233e1
Joshua Stern <jstern19@bu.edu>
2018-12-13 02:49:50 -0500

    Update kubeadm_build.sh

--- docker/kubeadm_build.sh
+++ docker/kubeadm_build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 echo "starting kubernetes..."
+
+#start up kubeadm
 swapoff -a
 kubeadm init --pod-network-cidr=192.168.0.0/16 > joining.txt
+
+#runs these to start using cluster as regular user
 mkdir -p $HOME/.kube
 echo "Y" | cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+
+#setup up CIDR
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
+#allow for pods to be scheduled on master node
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+#generate join.sh file that contains the command for adding worker nodes
+./add_worker.py
